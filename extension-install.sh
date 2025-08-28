7db69e82b31446d45cf0fe165cd0ffafc93941ea
vrwolf1004 <vrwolf.remote@gmail.com>
2025-08-28 16:56:26 +0900

    change info

--- extension-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#change execute : chmod +x ./vscode-extension-install.sh 
-#excute sh : ./extension-install.sh "FileName.txt"
-cat $1 | sed -n '2,$p' | while read extension || [[ -n $extension ]];
-do
-  code-server --install-extension $extension
-done
